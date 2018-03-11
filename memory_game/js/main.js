43e46b7a3a29efff89f33cfee1657dfc2ec451e6
Rhiannon Kaulitzke <rhiannonkaulitzke@Rhiannons-MacBook-Air.local>
2018-03-11 12:47:11 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,23 @@
-console.log("Up and running!");
+
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped Queen");
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped Queen");
-if (cardsInPlay.length === 2); {
+
+var checkForMatch = function (cardsInPlay) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
 	}
 }
+
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+if (cardsInPlay.length === 2); {
+	checkForMatch(cardsInPlay);
+	}
+}
+flipCard(0);
+flipCard(2);
 
 
